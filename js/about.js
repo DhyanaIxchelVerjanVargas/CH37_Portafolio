14fd16c5a712c4939c30490feacfd7b1ca493da3
dhyana <dhyana.ixchel@gmail.com>
2024-03-17 20:58:25 -0600

    Se le agregó la funcionalidad al botón de subir con JavaScript

--- js/about.js
+++ js/about.js
@@ -1,6 +1,16 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
+const btnScrollTop = document.getElementById("boton-arriba");
 
 navToggle.addEventListener("click", () =>{
 	navMenu.classList.toggle("nav-menu_visible");
 });
+
+//Le da la funcionalidad al botón de subir
+btnScrollTop.addEventListener('click', function(event){
+	event.preventDefault();
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth"
+	})
+})
